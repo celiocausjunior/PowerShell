8285be987be907ad72170128435429f33918590e
Celio Caus Junior <ccaus@uol.com.br>
2019-11-14 14:08:33 -0300

    Script para converter imagens Tif de forma sequencial

---
+++ conversor.ps1
@@ -0,0 +1,18 @@
+﻿Function converter{
+$oldfiles = Get-ChildItem
+$i = 1
+foreach ($newfile in $oldfiles)
+{
+if ($i -lt 10){
+Rename-Item $newfile.FullName -NewName ('0000'+$i+'.tif')
+$i++
+}
+elseif ($i -ge 10 -and $i -lt 100) {
+Rename-Item $newfile.FullName -NewName ('000'+$i+'.tif')
+$i++
+}
+else{
+Rename-Item $newfile.FullName -NewName ('00'+$i+'.tif')
+$i++}
+}
+}
